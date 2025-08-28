f326ba09b3aad8f71a693414129ae39967498ece
adelerpan-afk <adelerpan@gmail.com>
2025-08-28 10:15:29 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  // Boleh tambahkan opsi lain di sini kalau perlu
+};
+module.exports = nextConfig;
